8b41aa295f4d2c711c0d8f91b3b7e73e4b59201d
YUhari0901 <lnara1004l@gmail.com>
2023-01-18 11:12:18 +0900

    delayP함수

--- client/main.js
+++ client/main.js
@@ -1,6 +1,18 @@
 
 
-import {  } from "./lib/index.js";
+import { insertLast, xhrData } from "./lib/index.js";
+
+
+
+xhrData.get(
+  'https://jsonplaceholder.typicode.com/users/1',
+  (res) => {
+    console.log(res);
+  },
+  (err) =>{
+    console.log(err);
+  }
+)
 
   
   
